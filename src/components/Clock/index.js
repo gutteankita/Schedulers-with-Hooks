b318ea996c626d10eb7f9605afe1cf1896164aab
ankitagutte <ankitagutte2007@gmail.com>
2024-02-15 10:32:41 +0000

    first commit

---
+++ src/components/Clock/index.js
@@ -0,0 +1,28 @@
+import {useState, useEffect} from 'react'
+import {ClockContainer, Heading, ClockImage, Time} from './styledComponents'
+
+const Clock = () => {
+  const [date, setDate] = useState(new Date())
+  useEffect(() => {
+    const timeId = setInterval(() => {
+      setDate(new Date())
+    }, 1000)
+    console.log('sett')
+    return () => {
+      clearInterval(timeId)
+      console.log('cll')
+    }
+  }, [])
+  return (
+    <ClockContainer>
+      <Heading>Clock</Heading>
+      <ClockImage
+        src="https://assets.ccbp.in/frontend/hooks/clock-img.png"
+        alt="clock"
+      />
+      <Time> {date.toLocaleTimeString()} </Time>
+    </ClockContainer>
+  )
+}
+
+export default Clock
